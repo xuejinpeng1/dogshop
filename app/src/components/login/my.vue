549a4c654b227cf4240e0ddb2ed9481b4bbfb7a8
WangYandong <153508739@qq.com>
2019-09-21 12:57:48 +0800

    xxx

---
+++ app/src/components/login/my.vue
@@ -0,0 +1,42 @@
+<!-- 自定义组件 -->
+<template>
+  <!-- 根元素 -->
+  <div>
+    <!-- 快捷登录 -->
+    <div class="head">
+      <h3>编辑个人资料</h3>
+      <img class="img1" src="../../assets/myself/10.png" alt />
+    </div>
+    <mt-field class="name" label="昵称 :" placeholder="请输入用户名"></mt-field>
+    <span></span>
+    <mt-popup v-model="popupVisible" position="bottom">vgyvgh </mt-popup>
+  
+  </div>
+</template>
+<script>
+export default {
+  data:{
+    popupVisible:true
+  }
+};
+</script>
+
+
+<style scoped>
+/* 账号登录***************** */
+.img1 {
+  /* 左箭头样式 */
+  width: 0.6rem;
+  height: 0.6rem;
+  display: inline-block;
+  position: absolute;
+  top: 0.7rem;
+}
+.head h3 {
+  /* 字体样式 */
+  font-size: 0.4rem;
+  text-align: center;
+  margin-bottom: 0.5rem;
+  margin-top: 0.7rem;
+}
+</style>
---
+++ app/src/components/login/wu.vue
@@ -0,0 +1,49 @@
+<!-- 自定义组件 -->
+<template>
+  <!-- 根元素 -->
+  <div>
+    <!-- 快捷登录 -->
+    <div class="head">
+      <h3>账号登录</h3>
+      <img @click="myself" class="img1" src="../../assets/myself/10.png" alt />
+    </div>
+    <!-- 暂无数据图片 -->
+    <div>
+        <img class="img2" src="../../assets/myself/wu.png" alt="">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    methods:{
+        myself(){
+           this.$router.push("/myself");
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    /* 账号登录***************** */
+.img1 {
+  /* 左箭头样式 */
+  width: 0.6rem;
+  height: 0.6rem;
+  display: inline-block;
+  position: absolute;
+  top: 0.7rem;
+}
+.head h3 {
+  /* 字体样式 */
+  font-size: 0.4rem;
+  text-align: center;
+  margin-bottom: 0.5rem;
+  margin-top: 0.7rem;
+}
+/* 暂无数据图片 */
+.img2{
+    width:100%;
+    height:11.5rem;
+}
+</style>
